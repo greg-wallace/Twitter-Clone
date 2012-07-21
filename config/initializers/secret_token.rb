0e5f521c886739db5c7291cc4ab1e41c51361070
wazina <greg@icodeforu.com>
2012-07-21 12:57:22 -0700

    merge

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = '33cf1c85f1f690b4c0bcdc6c8d7c9e0176900a49f1589adcb8e265e8a5c21a800cbec266cd58ad57453484c3a630ae25fb6213cd898cb064eb808807100989f1'
+SampleApp::Application.config.secret_token = 'a2ec843ec2a6624bfb32e80334d2528c5820da3abed0abe8a6338e8b2ea8ccf19de65049b3ee508735cf532801e2ff2a94b89f0247fdc189d64e21aa2a904522'
