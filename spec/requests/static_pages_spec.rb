5e1e6ab84c1468f7f9a895d1132a808ac599143a
wazina <greg@icodeforu.com>
2012-07-21 17:07:44 -0700

    mid signup 7.2

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -61,4 +61,12 @@ describe "Static pages" do
       page.should have_selector 'title', text: full_title('')
     end
     
+    describe "profile page" do
+        let(:user) { FactoryGirl.create(:user) }
+        before { visit user_path(user) }
+
+        it { should have_selector('h1',    text: user.name) }
+        it { should have_selector('title', text: user.name) }
+      end
+    
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,6 +5,7 @@ describe "User pages" do
   subject { page }
 
   describe "signup page" do
+    let(:user) { FactoryGirl.create(:user) }
     before { visit signup_path }
 
     it { should have_selector('h1',    text: 'Sign up') }
