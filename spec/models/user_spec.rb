a14ab01aacc958697c07a54243ea2b17c33a06a4
wazina <greg@icodeforu.com>
2012-07-25 16:49:02 -0700

    starting 10.2.1

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -28,6 +28,7 @@ describe User do
   it { should be_valid }
   it { should respond_to(:admin) }
   it { should_not be_admin }
+  it { should respond_to(:microposts) }
 
     describe "with admin attribute set to 'true'" do
       before { @user.toggle!(:admin) }
@@ -116,6 +117,30 @@ describe User do
             before { @user.save }
             its(:remember_token) { should_not be_blank }
           end
+          
+          
+         describe "micropost associations" do
+
+           before { @user.save }
+               let!(:older_micropost) do 
+                 FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+               end
+               let!(:newer_micropost) do
+                 FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+               end
+
+        it "should have the right microposts in the right order" do
+          @user.microposts.should == [newer_micropost, older_micropost]
+        end
+        
+        it "should destroy associated microposts" do
+              microposts = @user.microposts
+              @user.destroy
+              microposts.each do |micropost|
+                Micropost.find_by_id(micropost.id).should be_nil
+        end
+      end
+  end
     
     
 end
