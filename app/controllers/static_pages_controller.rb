f4a0ff49068f192c19e85b1c7fafff80d685d874
wazina <greg@icodeforu.com>
2012-07-26 16:00:11 -0700

    no good

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,10 @@
 class StaticPagesController < ApplicationController
+  
   def home
-  end
-
+      @micropost = current_user.microposts.build if signed_in?
+    end
+    
+    
   def help
   end
   
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,10 +1,4 @@
 class Micropost < ActiveRecord::Base
-  attr_accessible :content
-  belongs_to :user
-  
-  validates :content, presence: true, length: { maximum: 140 }
+  attr_accessible :content, :user_id  
   validates :user_id, presence: true
-  
-  default_scope order: 'microposts.created_at DESC'
-  
 end
