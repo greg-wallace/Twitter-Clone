6329dce9f8effb31bee809ee43f879c40b98d738
wazina <greg@icodeforu.com>
2012-07-26 14:02:46 -0700

    ff

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,30 +1,66 @@
 require 'spec_helper'
 
-describe "User pages" do
-
-  subject { page }
+describe "Users" do
   
-  
-  describe "profile page" do
-      let(:user) { FactoryGirl.create(:user) }
-      let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
-      let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+  describe "signup" do
 
-      before { visit user_path(user) }
-
-      it { should have_selector('h1',    text: user.name) }
-      it { should have_selector('title', text: user.name) }
-
-      describe "microposts" do
-        it { should have_content(m1.content) }
-        it { should have_content(m2.content) }
-        it { should have_content(user.microposts.count) }
+    describe "failure" do
+      it "should not make a new user" do
+        lambda do
+          visit signup_path
+          fill_in "Name",         :with => ""
+          fill_in "Email",        :with => ""
+          fill_in "Password",     :with => ""
+          fill_in "Confirmation", :with => ""
+          click_button
+          response.should render_template('users/new')
+          response.should have_selector('div#error_explanation')
+        end.should_not change(User, :count)
       end
     end
-      
-      
-   
-     
+
+    describe "success" do
+      it "should make a new user" do
+        lambda do
+          visit signup_path
+          fill_in "Name",         :with => "Example User"
+          fill_in "Email",        :with => "user@example.com"
+          fill_in "Password",     :with => "foobar"
+          fill_in "Confirmation", :with => "foobar"
+          click_button
+          response.should have_selector('div.flash.success',
+                                        :content => "Welcome")
+          response.should render_template('users/show')
+        end.should change(User, :count).by(1)
+      end
+    end
+  end
+
+  describe "signin" do
+    
+    describe "failure" do
+      it "should not sign a user in" do
+        visit signin_path
+        fill_in "Email",    :with => ""
+        fill_in "Password", :with => ""
+        click_button
+        response.should have_selector('div.flash.error',
+                                      :content => "Invalid")
+        response.should render_template('sessions/new')
+      end
+    end
+    
+    describe "success" do
+      it "should sign a user in and out" do
+        user = Factory(:user)
+        visit signin_path
+        fill_in "Email",    :with => user.email
+        fill_in "Password", :with => user.password
+        click_button
+        controller.should be_signed_in
+        click_link "Sign out"
+        controller.should_not be_signed_in
+      end
+    end
   end
 end
-    
