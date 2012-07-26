1ac8ac13ba2078c03c3b713a8cf58efba10bc36a
wazina <greg@icodeforu.com>
2012-07-25 17:18:15 -0700

    10.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,10 @@ class UsersController < ApplicationController
   
 
 
-def show
-  @user = User.find(params[:id])
-end
+  def show
+      @user = User.find(params[:id])
+      @microposts = @user.microposts.paginate(page: params[:page])
+    end
 
 
 def new
